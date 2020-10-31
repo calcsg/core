d06a572ad85fc4852444fba81baf69e4df8d872e
turtle96 <angjazlyn@hotmail.com>
2020-11-01 00:44:30 +0800

    add general layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,56 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="container">
-    </div>
-  );
+    return (
+        <div>
+            <section className="hero">
+                <div className="hero-body">
+                    <div className="container">
+                        <img src={"/images/unicorn.svg"} className="logo image is-48x48"/>
+                        <h1 className="title">
+                            calc.sg
+                        </h1>
+                        <h2 className="subtitle">
+                            insert subtitle here
+                        </h2>
+                    </div>
+                </div>
+            </section>
+            {HomepageLayout()}
+        </div>
+    );
+}
+
+function HomepageLayout() {
+    return (
+        <div className="container">
+            <div className="columns">
+                <div className="column"/>
+                {HomepageBox("home", "Housing")}
+                {HomepageBox("grow", "Financial Independence")}
+                <div className="column"/>
+            </div>
+            <div className="columns">
+                <div className="column"/>
+                {HomepageBox("family", "Family")}
+                {HomepageBox("sunbed", "Luxury")}
+                <div className="column"/>
+            </div>
+        </div>
+    );
+}
+
+function HomepageBox(icon, label, link) {
+    return (
+        <div className="column">
+            <a href={link}>
+                <div className="content homepage-box">
+                    <img src={`/images/${icon}.svg`} className="image" alt={label}/>
+                    {label}
+                </div>
+            </a>
+        </div>
+    );
 }
 
 export default App;
